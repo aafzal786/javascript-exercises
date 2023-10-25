aa6d541651b2675cf64116c6be4e989424570211
aafzal786 <azeemafzal786@hotmail.co.uk>
2023-10-25 03:53:54 +0100

    Complete JavaScript Exercises
      - completed 01_helloWorld
      - completed 02_repeatString
      - completed 03_reverseString
      - completed 04_removeFromArray
      - completed 05_sumAll
      - completed 06_leapYears
      - completed 07_tempConversion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function (firstValue, secondValue) {
+    let arr = [];
+    let finalSum = 0;
+    if (firstValue < secondValue) {
+        for (let i = firstValue; i <= secondValue; i++) {
+            arr.push(i);
+        }
+        for (let i = 0; i < arr.length; i++) {
+            finalSum += arr[i];
+        }
+    } else if (firstValue > secondValue) {
+        for (let i = secondValue; i <= firstValue; i++) {
+            arr.push(i);
+        }
+        for (let i = 0; i < arr.length; i++) {
+            finalSum += arr[i];
+        }
+    }
+    if (finalSum < 0 || !Number.isInteger(firstValue) || !Number.isInteger(secondValue)) {
+        return "ERROR";
+    }
+    return finalSum;
 
 };
 
--- 07_tempConversion/README.md
+++ 07_tempConversion/README.md
@@ -6,6 +6,9 @@ convertToCelsius(32) // fahrenheit to celsius, should return 0
 
 convertToFahrenheit(0) // celsius to fahrenheit, should return 32
 ```
+convert F to C (x − 32) × 5/9
+convert C to F (x × 9/5 + 32)
+
 
 Because we are human, we want the result temperature to be rounded to one decimal place: i.e., `convertToCelsius(100)` should return `37.8` and not `37.77777777777778`.
 
